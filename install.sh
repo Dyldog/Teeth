535a2f91e9da76be1140adb9300dcbd4642576df
dylan-elliott <dylan@bournedigital.com.au>
2021-01-18 17:15:33 +1100

    Update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 # sudo apt-get update
 # sudo apt-get install clang
-sudo apt-get install libcurl4 libpython2.7 libpython2.7-dev
-wget https://swift.org/builds/swift-5.0.1-release/ubuntu1804/swift-5.0.1-RELEASE/swift-5.0.1-RELEASE-ubuntu18.04.tar.gz
-tar xzf swift-5.0.1-RELEASE-ubuntu18.04.tar.gz
-echo "export PATH=/usr/share/swift/usr/bin:$PATH" >> ~/.bashrc
+# sudo apt-get install libcurl4 libpython2.7 libpython2.7-dev
+wget https://swift.org/builds/swift-5.2.5-release/ubuntu1804/swift-5.2.5-RELEASE/swift-5.2.5-RELEASE-ubuntu18.04.tar.gz
+tar xzf swift-5.2.5-RELEASE-ubuntu18.04.tar.gz
+sudo mv swift-5.2.5-RELEASE-ubuntu18.04 /usr/share/swift
+# echo "export PATH=/usr/share/swift/usr/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
